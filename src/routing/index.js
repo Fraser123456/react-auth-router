e19a0b0b1a7ad79d79aa02ffe726d0bed44eb127
Fraser123456 <fraserac3@gmail.com>
2025-10-30 21:21:32 +0200

    fixed another bug

--- src/routing/index.js
+++ src/routing/index.js
@@ -1,14 +1,16 @@
+import { useRouter as useRouterHook } from "./Router";
+
 export { Router, useRouter, useNavigate } from "./Router";
 export { RouteGuard } from "./RouteGuard";
 export { Routes } from "./Routes";
 
 // Individual hooks for better performance
 export const useParams = () => {
-  const { params } = useRouter();
+  const { params } = useRouterHook();
   return params;
 };
 
 export const useQuery = () => {
-  const { query } = useRouter();
+  const { query } = useRouterHook();
   return query;
 };
